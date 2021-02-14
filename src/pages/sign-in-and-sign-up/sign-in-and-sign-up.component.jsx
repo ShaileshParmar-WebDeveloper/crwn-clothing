b198079ba4c1b371e7f3ea701b9ee65b758ae1ad
Shailesh Parmar <shailesh.parmar.webdev@gmail.com>
2021-02-14 19:16:25 +0530

    added sign up component with sign in with email and password facility

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import "./sign-in-and-sign-up.style.scss";
 import SignIn from "../../component/sign-in/sign-in.component";
+import SignUp from "../../component/sign-up/sign-up.component";
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
